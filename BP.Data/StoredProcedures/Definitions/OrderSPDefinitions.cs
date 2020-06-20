f56c1ad0d74fe10bb70c29c1e9f8a7ee9af2090c
Jiri Batulka <j.batulka@gmail.com>
2020-06-20 10:48:16 +0200

    BP.Shared deleted

--- BP.Data/StoredProcedures/Definitions/OrderSPDefinitions.cs
+++ BP.Data/StoredProcedures/Definitions/OrderSPDefinitions.cs
@@ -7,9 +7,40 @@ namespace BP.StoredProcedures.Definitions
     internal class OrderSPDefinitions
     {
         public static Dictionary<string, string> Definitions = new Dictionary<string, string>();
+        public static string AddOrder = "AddOrder";
+
         static OrderSPDefinitions()
         {
+            CreateAddOrder();
+        }
 
+        private static void CreateAddOrder()
+        {
+            Definitions[AddOrder] =
+                @"CREATE PROCEDURE [dbo].[AddOrder]
+                        @OrderID uniqueidentifier,
+                        @StartTime datetime2(7),
+                        @EndTimeEstimate datetime2(7),
+                        @StartLocationLat float,
+                        @StartLocationLng float,
+                        @EndLocationLat float,
+                        @EndLocationLng float,
+                        @CustomerID uniqueidentifier,
+                        @VehicleID uniqueidentifier
+                    AS
+                    BEGIN
+                        INSERT INTO [dbo].[Orders]  
+                            (DriverID,  
+                             FirstName,  
+                             Surname,  
+                             PhoneNumber)
+  
+                        VALUES ( 
+                             @DriverID,  
+                             @FirstName,  
+                             @Surname,  
+                             @PhoneNumber)  
+                    END";
         }
     }
 }
